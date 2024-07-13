25eea15cc3634125ea3b0c09f1532b62bb2f9a23
remiangelo <justryeoi@gmail.com>
2024-07-13 22:16:06 +0700

    Datepicker / calendar added

--- components/customformfield.tsx
+++ components/customformfield.tsx
@@ -17,6 +17,9 @@ import Image from "next/image"
 import 'react-phone-number-input/style.css'
 import PhoneInput from "react-phone-number-input"
 import {E164Number} from "libphonenumber-js";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 interface CustomProps {
     control: Control<any>,
@@ -72,7 +75,18 @@ const RenderField = ({field, props }: {field: any; props: CustomProps;}) => {
             )
         case FormFieldType.DATE_PICKER:
             return (
-                <div className="flex rounded-md border border-dark-500 bg-dark-400"></div>
+                <div className="flex rounded-md border border-dark-500 bg-dark-400">
+                    <Image
+                        src="/assets/icons/calendar.svg"
+                        height={24}
+                        width={24}
+                        alt="calender"
+                        className="ml-2"
+                    />
+                    <FormControl>
+                        <DatePicker selected={field.value} onChange={(date) => field.onChange(date)} />
+                    </FormControl>
+                </div>
             )
         default:
         break;
