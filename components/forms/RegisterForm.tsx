16c8facf65994cceb63431aeb5809089901b8105
remiangelo <justryeoi@gmail.com>
2024-07-14 10:25:55 +0700

    Personal Info Complete

--- components/forms/RegisterForm.tsx
+++ components/forms/RegisterForm.tsx
@@ -121,9 +121,47 @@ const RegisterForm = ({ user }: { user: User }) => {
                             </FormControl>
                         )}
                     />
-
-
                 </div>
+
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="address"
+                    label="Address"
+                    placeholder="1 Infinite Loop"
+                />
+                    <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="occupation"
+                    label="Occupation"
+                    placeholder="Astronaut"
+                />
+                
+                <div className="flex flex-col gap-6 xl:flex-row">
+                    <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="emergencyContactName"
+                    label="Emergency Contacts Name"
+                    placeholder="Guardian's Name"
+                />
+
+                    <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="emergencyContactNumber"
+                    label=" Emergency Contact Number"
+                    placeholder="(555) 123-4567"
+                />
+                </div>
+                </div>
+                <section className="space-y-6">
+                    <div className="mb-9 space-y-1">
+                    <h2 className="sub-header">Medical Information</h2>
+                    </div>
+                </section>
 
                 <SubmitButton isLoading={isLoading}>Get Started</SubmitButton>
             </form>
